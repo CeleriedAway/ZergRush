18f02f2f584f958dd45f06d5f3fe2298f7cf42ea
Aleksandr Pyatkov <myinkmonster@gmail.com>
2021-10-09 14:37:54 +0300

    add SortReactive to reactive collection

--- Assets/ZergRush/Reactive/ReactiveCore/ReactiveCollection.cs
+++ Assets/ZergRush/Reactive/ReactiveCore/ReactiveCollection.cs
@@ -1289,6 +1289,76 @@ namespace ZergRush.ReactiveCore
         }
         
         [DebuggerDisplay("{this.ToString()}")]
+        public class SortedCollection<T> : AbstractCollectionTransform<T>
+        {
+            public readonly Func<T, T, int> sorter;
+            public readonly IReactiveCollection<T> collection;
+            
+            public SortedCollection(IReactiveCollection<T> collection, Func<T, T, int> predicate)
+            {
+                this.collection = collection;
+                this.sorter = predicate;
+            }
+
+            void Insert(int realIndex, T item)
+            {
+                buffer.InsertSorted(sorter, item);
+            }
+
+            void Remove(int realIndex)
+            {
+                var item = collection[realIndex];
+                buffer.Remove(item);
+            }
+
+            void Process(IReactiveCollectionEvent<T> e)
+            {
+                switch (e.type)
+                {
+                    case ReactiveCollectionEventType.Reset:
+                        RefillRaw();
+                        break;
+                    case ReactiveCollectionEventType.Insert:
+                        Insert(e.position, e.newItem);
+                        break;
+                    case ReactiveCollectionEventType.Remove:
+                        Remove(e.position);
+                        break;
+                    case ReactiveCollectionEventType.Set:
+                        //TODO make proper set event resolve if needed
+                        Remove(e.position); 
+                        Insert(e.position, e.newItem);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            protected override IDisposable StartListenAndRefill()
+            {
+                RefillRaw();
+                return collection.update.Subscribe(Process);
+            }
+
+            protected override void RefillRaw()
+            {
+                buffer.Clear();
+                var coll = collection;
+                for (int i = 0; i < coll.Count; i++)
+                {
+                    var item = coll[i];
+                    buffer.InsertSorted(sorter, item);
+                }
+            }
+        }
+
+        public static IReactiveCollection<T> SortReactive<T>(this ReactiveCollection<T> collection,
+            Func<T, T, int> comparator)
+        {
+            return new SortedCollection<T>(collection, comparator);
+        }
+        
+        [DebuggerDisplay("{this.ToString()}")]
         public class FilteredCollection<T> : AbstractCollectionTransform<T>
         {
             readonly Func<T, bool> predicate;
