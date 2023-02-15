939224a12302f694c05b41c19864e31c0d3a1d68
Alexandr Pyatkov <myinkmonster@gmail.com>
2023-02-15 16:46:50 +0200

    remove redundant stuff

--- Assets/ZergRush/PureCSharp/Tools/RandomExtensions.cs
+++ Assets/ZergRush/PureCSharp/Tools/RandomExtensions.cs
@@ -8,12 +8,6 @@ namespace ZergRush
 {
     public static partial class RandomExtensions
     {
-        public static T TakeRandom<T>(this List<T> list, ZergRandom random)
-        {
-            var i = random.Range(0, list.Count);
-            return list.TakeAt(i);
-        }
-        
         public static List<float> NormalizeFloatRange(this IEnumerable<float> range)
         {
             var list = range.ToList();
