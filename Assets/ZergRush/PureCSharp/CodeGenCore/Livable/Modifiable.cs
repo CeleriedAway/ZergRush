0ed4a146615c55313f0dae706a85246a87a67890
sergan4ik <klochaksergiy@knu.ua>
2024-01-11 15:16:16 +0200

    last value modification fix

--- Assets/ZergRush/PureCSharp/CodeGenCore/Livable/Modifiable.cs
+++ Assets/ZergRush/PureCSharp/CodeGenCore/Livable/Modifiable.cs
@@ -21,6 +21,7 @@ namespace ZergRush.ReactiveCore
         protected List<TModification> modifications = new List<TModification>();
         EventStream<TVal> changed = new EventStream<TVal>();
 
+        [GenInclude]
         public TVal baseValue
         {
             get { return baseVal; }
@@ -122,6 +123,7 @@ namespace ZergRush.ReactiveCore
 
     public class LastValueModification<T> : Modifiable<T, T>
     {
+        public LastValueModification() : base(default(T)) { }
         public LastValueModification(T baseVal) : base(baseVal)
         {
         }
