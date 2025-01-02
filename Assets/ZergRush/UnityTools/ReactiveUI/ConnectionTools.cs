755897764486433f028ab3a76a0b7bce763fc3a6
Alexandr Pyatkov <myinkmonster@gmail.com>
2025-01-02 10:18:36 +0200

    add api

--- Assets/ZergRush/UnityTools/ReactiveUI/ConnectionTools.cs
+++ Assets/ZergRush/UnityTools/ReactiveUI/ConnectionTools.cs
@@ -4,6 +4,15 @@ namespace ZergRush
 {
     public static class ConnectionTools
     {
+        public static GameObject Instantiate(this Connections connections, GameObject prefab, 
+            Transform parent, bool worldPosStay = false)
+        {
+            var obj = GameObject.Instantiate(prefab, parent, worldPosStay);
+            connections += new AnonymousDisposable(() => {
+                if (obj) GameObject.Destroy(obj);
+            });
+            return obj;
+        }
         public static T Instantiate<T>(this Connections connections, T prefab, Transform parent, bool worldPosStay = false) where T : Component
         {
             var obj = GameObject.Instantiate(prefab, parent, worldPosStay);
