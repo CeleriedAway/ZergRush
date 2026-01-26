9d3b3470eb2e741f8fd8312ae38103e662fabcdd
Alexandr Pyatkov <myinkmonster@gmail.com>
2026-01-26 14:25:23 +0200

    cell transform

--- Assets/ZergRush/PureCSharp/ReactiveCore/CellAPI/Cell.AuxTools.cs
+++ Assets/ZergRush/PureCSharp/ReactiveCore/CellAPI/Cell.AuxTools.cs
@@ -13,6 +13,14 @@ namespace ZergRush.ReactiveCore
             connectionSink(eventStream.Subscribe(val => cell.value = val));
             return cell;
         }
+        
+        // This version of Hold works well only when cell is subscribed right after creation
+        // If not subscribed right away then it will return initial value until subscription and first event
+        public static ICell<T> HoldWhenSubscribed<T>(this IEventStream<T> eventStream, T initial)
+        {
+            var cell = new HoldCell<T>(eventStream, initial);
+            return cell;
+        }
 
         /// Makes connection to cell and creates another cell as intermediate buffer.
         /// It can be used for optimization purposes when you need multiple connections to complex cell
@@ -24,9 +32,53 @@ namespace ZergRush.ReactiveCore
             return materializedCell;
         }
 
+        /// Creates special intermediate buffer for cell, that activates on first connection.
+        /// It can be used for optimization purposes when you need multiple connections to complex cell
+        /// Similar to Materialize but lazy.
+        public static ICell<T> Cache<T>(this ICell<T> cell)
+        {
+            return new BufferCell<T>(cell);
+        }
+
         public static ICell<T> ToStaticCell<T>(this T val)
         {
             return new StaticCell<T>(val);
+        }
+
+        class BufferCell<T> : BufferCellTransform<T>
+        {
+            ICell<T> source;
+            public BufferCell(ICell<T> source)
+            {
+                this.source = source;
+            }
+            protected override IDisposable StartListenAndRefill()
+            {
+                return source.ListenUpdates(val => buffer.value = val);
+            }
+            protected override void RefillRaw()
+            {
+                buffer.value = source.value;
+            }
+        }
+        
+        class HoldCell<T> : BufferCellTransform<T>
+        {
+            IEventStream<T> eventStream;
+            public HoldCell(IEventStream<T> eventStream, T initial)
+            {
+                buffer.value = initial;
+                this.eventStream = eventStream;
+            }
+
+            protected override IDisposable StartListenAndRefill()
+            {
+                return eventStream.Subscribe(val => buffer.value = val);
+            }
+
+            protected override void RefillRaw()
+            {
+            }
         }
     }
 }
